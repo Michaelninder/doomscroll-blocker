8dc92bbcdb8722c5e9844513c53d471d5661471d
Michaelninder <fabianeternis@gmail.com>
2026-01-07 21:55:42 +0100

    feat: Add .blocker-content to #blockerBanner

--- blocker.js
+++ blocker.js
@@ -1,4 +1,10 @@
 const blockerBanner = document.createElement('div');
 blockerBanner.id = "blockerBanner";
+blockerBanner.innerHTML = `
+    <div class="blocker-content">
+        <h1>Focus Mode Active</h1>
+        <p>This site has been blocked by <strong>Doomscroll Blocker</strong></p>
+    </div>
+`;
 
 document.documentElement.appendChild(blockerBanner);
