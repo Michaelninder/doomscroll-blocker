20a2f8335684d7581e395a796ff1d552857b3936
Michaelninder <fabianeternis@gmail.com>
2026-01-08 08:55:34 +0100

    feat: Update settings (add tabs and reset-popover). Add resetOptions(), storeBlockedSites() and more. Also: update blocker.js to only block sites that have items from the localStorage.getItem("blockedSites") ...

--- blocker.js
+++ blocker.js
@@ -1,10 +1,21 @@
-const blockerBanner = document.createElement('div');
-blockerBanner.id = "blockerBanner";
-blockerBanner.innerHTML = `
+//const _blockedSites = [ "x.com", "youtube.com/shorts", "instagram.com", "facebook.com", "tiktok.com", "fabianternis.de"];
+//localStorage.setItem("blockedSites", JSON.stringify(_blockedSites));
+
+const blockedSites = JSON.parse(localStorage.getItem("blockedSites")) || [];
+const currentUrl = window.location.href;
+//console.log(blockedSites)
+
+const isBlocked = blockedSites.some((site) => currentUrl.includes(site));
+
+if (isBlocked) {
+    const blockerBanner = document.createElement("div");
+    blockerBanner.id = "blockerBanner";
+    blockerBanner.innerHTML = `
     <div class="blocker-content">
         <h1>Focus Mode Active</h1>
         <p>This site has been blocked by <strong>Doomscroll Blocker</strong></p>
     </div>
 `;
 
-document.documentElement.appendChild(blockerBanner);
+    document.documentElement.appendChild(blockerBanner);
+}
