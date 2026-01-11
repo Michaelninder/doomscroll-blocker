c64d5e4abe6efba449de77e96f0e0488bb21fac5
Michaelninder <fabianeternis@gmail.com>
2026-01-11 18:00:46 +0100

    Copy code from StackOverflow to add disableScroll() if isBlocked

--- blocker.js
+++ blocker.js
@@ -12,6 +12,55 @@ function trimURL(url) {
   return url.endsWith("/") ? url.slice(0, -1) : url;
 };
 
+function functionmThatIsAnyhowAlsoNotFunctional() {
+    // Source - https://stackoverflow.com/a/13883978
+    // Posted by vogdb, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-01-11, License - CC BY-SA 4.0
+
+    var link = document.createElement('link');
+    link.setAttribute('rel', 'stylesheet');
+    link.setAttribute('href', 'blocked-site.css');
+    document.head.appendChild(link);
+}
+
+// Source - https://stackoverflow.com/a/4770179
+// Posted by gblazex, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-01-11, License - CC BY-SA 4.0
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+  if (keys[e.keyCode]) {
+    preventDefault(e);
+    return false;
+  }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+  window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+    get: function () { supportsPassive = true; } 
+  }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+// call this to Disable
+function disableScroll() {
+  window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+  window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+  window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+  window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
 chrome.storage.local.get(["blockedSites"], (result) => {
   const blockedSites = result.blockedSites || [];
   const currentUrl = window.location.href;
@@ -33,5 +82,7 @@ chrome.storage.local.get(["blockedSites"], (result) => {
 
     document.documentElement.appendChild(blockerBanner);
     document.documentElement.classList.add("whiy-is-this-not-working");
+    functionmThatIsAnyhowAlsoNotFunctional();
+    disableScroll();
   }
 });
